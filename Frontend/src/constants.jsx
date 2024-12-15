dcf16724191866c6bdfddd05f297ce8359dd9136
skoop7 <skobarne7@gmail.com>
2024-12-15 18:05:28 +0530

    Connection2

--- Frontend/src/constants.jsx
+++ Frontend/src/constants.jsx
@@ -1,8 +1,8 @@
 const COMPREHENSION = "comprehension";
 const CLOZE = "cloze";
 const CATEGORY = "category";
-const SERVER_DEV_URL = "http://localhost:4000";
+// const SERVER_DEV_URL = "http://localhost:4000";
 const SERVER_PROD_URL = "https://super-assistant-1.onrender.com";
-const SERVER_URL = SERVER_DEV_URL;
+const SERVER_URL = SERVER_PROD_URL;
 
 export { COMPREHENSION, CATEGORY, CLOZE, SERVER_URL };
